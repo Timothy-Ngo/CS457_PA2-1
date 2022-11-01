aa65defe4e3e4ae93879cdd484594efdbb5f9b8d
mezamartinez-kimberly <kmezamartinez@nevada.unr.edu>
2022-11-01 02:57:38 -0700

    mod not done yet

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+---README
 # CS457_PA2
-database management project 2
+Author: Kimberly Meza Martinez
+        10/21/22
+
+Project Title: Basic Data Manipulation
+---This project is for CS457 Database Management at the Department of Computer Science and Engineering, University of Nevada, Reno.
+---This program allows the user to manage data in table by inserting, deleting, modifying and querying.
+
+Getting Started
+---This project currently runs using the layout python3 database.py PA2_test.txt. In the future it will run using the sql format including the > character.
+
+---System Design
--- database.py
+++ database.py
@@ -78,6 +78,23 @@ def selectTbl(valueInput):  # display contents of tbl(aka file)
         print('!Failed to query table', tblToView, 'becaue it does not exist.')
 
 
+def selectCategory(table, data):        #selecting multiple stuff to view COME BACK to function
+    tblToView = table
+    string = data
+    firstCetegory = string[0]
+    secondCetegory = string[1]
+    whereCategory = string[5]
+
+    tblExists = os.path.exists(tblToView)
+    if(tblExists == True):
+        fileTbl = open(tblToView, 'r')  # open file for reading
+        tblContent = fileTbl.read()  # gets strings in file to save to variable
+        print(tblContent)  # prints variable contents
+        fileTbl.close()  # close file
+    else:
+        print('!Failed to query table', tblToView, 'becaue it does not exist.')
+
+
 def alterTbl(valueInput, newValues):  # edit table adds to end of file
     tblToAlter = valueInput  # set value input to tblToAlter which is file
     addToTblValues = newValues  # set new values to addToTblValues
@@ -91,12 +108,13 @@ def alterTbl(valueInput, newValues):  # edit table adds to end of file
 def insertIntoTbl(table, newValues):  # insert into table
     tblToInsert = table
     addToTblValues = newValues  # set new values to addToTblValues
-    fileTbl = open(tblToInsert, 'a')        #might need to add new line in bewteen the nextl line and this one 
+    fileTbl = open(tblToInsert, 'a')        #might need to add new line in bewteen the nextl line and this one
+    fileTbl.write('\n')
     fileTbl.write("".join(addToTblValues))
     fileTbl.close()
     print('1 new record inserted.')
 
-def updateTable(table, string, count):      #update table
+def updateTable(table, string):      #update table
     tblToUpdate = table
     data = string
 
@@ -108,20 +126,21 @@ def updateTable(table, string, count):      #update table
 
     nameName = 'name'
     priceName = 'price'
-
+    
     if(setCategory == nameName and whereCategory == nameName):
-        with open(tblToUpdate, 'r') as file:     #read in file
-            data = file.read()
-        data = data.replace(whereValue, setValue)     #replaces values where is old val, set is new val
-        with open(tblToUpdate, 'w') as file:     #writes new values to same file
-            file.write(data)
+        print('here')
+        with open(tblToUpdate) as f:
+            fileData = f.read().replace(whereValue, setValue)       #replaces values where is old val, set is new val
+        with open(tblToUpdate, 'w') as f:
+            f.write(fileData)
+        
     elif(setCategory == priceName and whereCategory == nameName):
         with open(tblToUpdate, 'r') as file:       #open file to read
             with open('temp.txt', 'w') as newFile:      #open file to write
                 for line in file:       #loop through file to write only lines that do not contain the mathing word
                     if whereValue in line:
                         line[2] = setValue
-                        ouput.write(line)
+                        newFile.write(line)
 
 def deleteFrom(table, string):
     tblToDeleteFrom = table
@@ -135,7 +154,7 @@ def deleteFrom(table, string):
             with open('temp.txt', 'w') as newFile:      #open file to write
                 for line in file:       #loop through file to write only lines that do not contain the mathing word
                     if searchFor not in line.strip('\n'):
-                        ouput.write(line)
+                        newFile.write(line)
 
         os.replace('temp.txt', tblToDeleteFrom)     #replace files
     elif(categoryName == priceName):
@@ -144,7 +163,7 @@ def deleteFrom(table, string):
                 for line in file:       #loop through file to write only lines that do not contain the mathing word
                     if line > searchFor:
                         line.strip('\n')
-                        ouput.write(line)
+                        newFile.write(line)
 
 
 def main():
@@ -152,12 +171,16 @@ def main():
     dbTestFile = sys.argv[1]  # save testfile name into variable
     f = open(dbTestFile, 'r')  # open file for reading
     commandLines = f.readlines()  # read lines into strings
-
+    num = 0
     for line in commandLines:  # loop through each line
         # line alterings
         currentWorkingLine = line  # saves line into varriable of single line
-        currentWorkingLine = currentWorkingLine.replace(';', '')
+        #currentWorkingLine = currentWorkingLine.replace('\n', '')      #need to account for end lin eventually!!!!
+        currentWorkingLine = currentWorkingLine.replace(';', '')        #removes semicolon at end of each testline
+        currentWorkingLine = currentWorkingLine.replace('(', ' ')       #replaces () characters with spaces to split by next
+        currentWorkingLine = currentWorkingLine.replace(')', ' ')
         arrayLines = currentWorkingLine.split()  # split lines by spaces
+        
         # checkNames
         createName = 'CREATE'
         dropName = 'DROP'
@@ -165,14 +188,17 @@ def main():
         tbName = 'TABLE'
         useName = 'USE'
         selectName = 'SELECT'
+        starName = '*'
+        selectLowName = 'select'
         alterName = 'ALTER'
         exitName = '.EXIT'
+        exitLowName = '.exit'
         commentName = '--'
         name = ''
         insertName = 'insert'
         updateName = 'update'
         deleteName = 'delete'
-
+        newLineName = '\n'
         # if statements to call functions
         if(arrayLines[0] == createName and arrayLines[1] == dbName):  # create db
             name = arrayLines[2]
@@ -182,9 +208,7 @@ def main():
             name = arrayLines[2]
             # editing data info to put into file
             data = " ".join(arrayLines[3:])
-            firstEditData = data[:-1]
-            secondEditData = firstEditData[1:]
-            finalData = secondEditData.replace(',', ' |')
+            finalData = data.replace(',', ' |')
             createTbl(finalData, name)
 
         elif(arrayLines[0] == dropName and arrayLines[1] == dbName):  # drop db
@@ -199,9 +223,15 @@ def main():
             name = arrayLines[1]
             useDb(baseDirectory, name)
 
-        elif(arrayLines[0] == selectName):  # select
+        elif((arrayLines[0] == selectName and arrayLines[1] == starName) or (arrayLines[0] == selectLowName and arrayLines[1] == starName)):  # select
             name = arrayLines[3]
             selectTbl(name)
+
+        elif(arrayLines[0] == selectName):      #selecting multiple
+            data = " ".join(arrayLines[1:])
+            data = data.replace(',', '')       #replaces , w nothing
+            name = data[3]
+            selectCategory(name, data)#need to do 
 
         elif(arrayLines[0] == alterName and arrayLines[1] == tbName):  # alter tbl
             name = arrayLines[2]
@@ -212,10 +242,8 @@ def main():
         elif(arrayLines[0] == insertName):  # insert into tbl
             name = arrayLines[2]
             data = " ".join(arrayLines[4:])  # combines data to send into function
-            firstEditData = data[:-1]       #removes last )
-            secondEditData = firstEditData[1:]      #removes first ()
-            thirdData = secondEditData.replace(',', ' |')       #replaces , w " |"
-            finalData = thirdData.replace("'", "")      #replaces apostrophe w nothing
+            data = data.replace(',', ' |')       #replaces , w " |"
+            finalData = data.replace("'", "")      #replaces apostrophe w nothing
             insertIntoTbl(name, finalData)
 
         elif(arrayLines[0] == updateName):      #update table
@@ -223,9 +251,8 @@ def main():
             data = " ".join(arrayLines[3:])   #join = and rest to pass into function
             finalData = data.replace("'", "")
             updateTable(name, finalData)
-            count = 0
-            count += 1
-            print(count, 'records modified.')
+            num += 1
+            print(num, 'records modified.')
 
         elif(arrayLines[0] == deleteName):
             name = arrayLines[2]        #table name
@@ -233,7 +260,7 @@ def main():
             finalData = data.replace("'", "")
             deleteFrom(name, finalData)
 
-        elif(arrayLines[0] == exitName):  # exit
+        elif(arrayLines[0] == exitName or arrayLines[0] == exitLowName):  # exit
             print('All done.')
             quit()
 
